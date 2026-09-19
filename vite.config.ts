fdd045979b5172bf32394625be932e967630e355
desigan362-oss <desigan362@gmail.com>
2026-09-19 18:14:49 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  tanstackStart: {
-    server: { entry: "server" },
-    prerender: {
-      enabled: true,
-      crawlLinks: false,
-    },
-  },
+  base: "/design-m-portfolio/",
+  plugins: [react()],
 });
